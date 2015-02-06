a25cf5c49cf20cca73ae634ca24921ac55f9993a
Peter Mouland <peter.mouland@gmail.com>
2015-02-06 15:51:31 +0000

    v0.2.2

--- scripts/hash-manager.requirejs.js
+++ scripts/hash-manager.requirejs.js
@@ -1,5 +1,123 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var utils = require('./utils');
+var state = { css: {} };
+var html = document.documentElement;
+var toolkitClasses = ["no-touch", "touch-device"];
+var vendorPrefix = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'];
+var classes = { hasNot: toolkitClasses[0], has: toolkitClasses[1] };
+
+function attachClasses() {
+    var arrClasses = html.className.split(' ');
+    arrClasses.push(touch() ? classes.has : classes.hasNot);
+    html.className = arrClasses.join(' ');
+}
+
+function translate3d() {
+    var transforms = {
+            'webkitTransform': '-webkit-transform',
+            'OTransform': '-o-transform',
+            'msTransform': '-ms-transform',
+            'MozTransform': '-moz-transform',
+            'transform': 'transform'
+        },
+        body = document.body || document.documentElement,
+        has3d,
+        div = document.createElement('div'),
+        t;
+    body.insertBefore(div, null);
+    for (t in transforms) {
+        if (transforms.hasOwnProperty(t)) {
+            if (div.style[t] !== undefined) {
+                div.style[t] = "translate3d(1px,1px,1px)";
+                has3d = window.getComputedStyle(div).getPropertyValue(transforms[t]);
+            }
+        }
+    }
+    body.removeChild(div);
+    state.css.translate3d = (has3d !== undefined && has3d.length > 0 && has3d !== "none");
+    return state.css.translate3d;
+}
+
+function supportsCSS(property) {
+    if (state.css[property]) {
+        return state.css[property];
+    }
+    if (property === 'translate3d') {
+        return translate3d(property);
+    }
+    var style = html.style;
+    if (typeof style[property] == 'string') {
+        state.css[property] = true;
+        return true;
+    }
+    property = property.charAt(0).toUpperCase() + property.substr(1);
+    for (var i = 0; i < vendorPrefix.length; i++) {
+        if (typeof style[vendorPrefix[i] + property] == 'string') {
+            state.css[property] = true;
+            return state.css[property];
+        }
+    }
+    state.css[property] = false;
+    return state.css[property];
+}
+
+function css(el, property) {
+    if (!property) {
+        return supportsCSS(el);
+    }
+    var strValue = "";
+    if (document.defaultView && document.defaultView.getComputedStyle) {
+        strValue = document.defaultView.getComputedStyle(el, "").getPropertyValue(property);
+    } else if (el.currentStyle) {
+        property = property.replace(/\-(\w)/g, function (strMatch, p1) {
+            return p1.toUpperCase();
+        });
+        strValue = el.currentStyle[property];
+    }
+    return strValue;
+}
+
+function touch() {
+    state.touch = (typeof window.ontouchstart !== "undefined");
+    return state.touch;
+}
+
+function getElementOffset(el) {
+    return {
+        top: el.getBoundingClientRect().top + window.pageYOffset - document.documentElement.clientTop,
+        left: el.getBoundingClientRect().left + window.pageXOffset - document.documentElement.clientLeft
+    };
+}
+
+function elementVisibleBottom(el) {
+    if (el.length < 1)
+        return;
+    var elementOffset = getElementOffset(el);
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    return (elementOffset.top + el.offsetHeight <= scrollTop + document.documentElement.clientHeight);
+}
+
+function elementVisibleRight(el) {
+    if (el.length < 1)
+        return;
+    var elementOffset = getElementOffset(el);
+    return (elementOffset.left + el.offsetWidth <= document.documentElement.clientWidth);
+}
+
+attachClasses();
+
+module.exports = {
+    _attachClasses: attachClasses,
+    _state: state,
+    css: css,
+    touch: touch,
+    elementVisibleBottom: elementVisibleBottom,
+    elementVisibleRight: elementVisibleRight
+};
+
+if (typeof skyComponents === "undefined") window.skyComponents = {};
+skyComponents.detect = module.exports;
+},{}],2:[function(require,module,exports){
+var utils = require('../utils/event-helpers');
 var timeout = { resize: null };
 
 function bindEvents() {
@@ -72,7 +190,22 @@ module.exports = {
 if (typeof skyComponents === "undefined") window.skyComponents = {};
 skyComponents.event = module.exports;
 
-},{"./utils":2}],2:[function(require,module,exports){
+},{"../utils/event-helpers":4}],3:[function(require,module,exports){
+var version  = require('./utils/version');
+var event  = require('./api/event');
+var detect  = require('./api/detect');
+
+module.exports = {
+    version: version,
+    event: event,
+    detect: detect
+}
+
+if (typeof skyComponents === "undefined") window.skyComponents = {};
+skyComponents['version'] = version;
+skyComponents['event'] = event;
+skyComponents['detect'] = detect;
+},{"./api/detect":1,"./api/event":2,"./utils/version":5}],4:[function(require,module,exports){
 var eventRegistry = {};
 var state = {    };
 var browserSpecificEvents = {
@@ -179,8 +312,11 @@ module.exports = {
     addEventListener: addEventListener,
     removeEventListener: removeEventListener
 };
-},{}],3:[function(require,module,exports){
-var event = require('../../bower_components/bskyb-event/src/js/event');
+},{}],5:[function(require,module,exports){
+module.exports = "0.0.3";
+},{}],6:[function(require,module,exports){
+var core = require('../../bower_components/bskyb-core/src/scripts/core');
+var event = core.event;
 
 var vars = {
     globalHashList: {},
@@ -308,7 +444,7 @@ module.exports = {
 
 if (typeof skyComponents === "undefined") window.skyComponents = {};
 skyComponents['hash-manager'] = module.exports;
-},{"../../bower_components/bskyb-event/src/js/event":1}],4:[function(require,module,exports){
+},{"../../bower_components/bskyb-core/src/scripts/core":3}],7:[function(require,module,exports){
 var local; local['hash-manager'] = require('./hash-manager');
 
 if (typeof window.define === "function" && window.define.amd) {
@@ -317,4 +453,4 @@ if (typeof window.define === "function" && window.define.amd) {
         return local['hash-manager'];
     });
 }
-},{"./hash-manager":3}]},{},[4]);
+},{"./hash-manager":6}]},{},[7]);
