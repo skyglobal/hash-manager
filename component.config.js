ab2c4b83f5095634a8c7b7a5e9bfb05230388806
Peter Mouland <peter.mouland@gmail.com>
2015-02-06 15:07:19 +0000

    Now using core component

--- component.config.js
+++ component.config.js
@@ -3,62 +3,54 @@ var pkg = require('./package.json');
 
 module.exports = {
     bower: bower,
-    buildTool: 'gulp', //or grunt
-    buildStyles: 'sass', // or less
-    buildHTML: 'html-concat', // moustache or assemble or jekyll
-    buildScripts: 'browserify', // or requirejs
-    serve: '_site',
-    release: 'aws', // or false,
+    buildFonts: true, // or false. Set to false if you are doing your own thing in the fonts directory
+    buildTool: 'gulp', // grunt not yet available
+    buildStyles: 'sass', // less not yet available
+    buildHTML: 'html-concat', // moustache or assemble or jekyll not yet available
+    buildScripts: 'browserify', // or requirejs not yet available
+    release: 'aws', /// or 'aws',
     releaseConfig: { //add you release config here... this is for AWS
         bucket: process.env.AWS_SKYGLOBAL_BUCKET,
-        key: process.env.AWS_ACCESS_KEY_ID,
+        accessKey: process.env.AWS_ACCESS_KEY_ID,
         secret: process.env.AWS_SECRET_ACCESS_KEY,
         region: process.env.AWS_REGION,
-        directoryPrefix: 'components/'
+        directoryPrefix: 'components/' //prefix your target release destination
     },
     test: 'karma', //or mocha
+    testConfig: { // where your tests config, specs and reports are saved
+        root: './test',
+        specs: './test/specs',
+        config: './test/karma.conf.js',
+        summary: './test/coverage/summary.json'
+    },
+    serve: '_site', // can be a node app like below
+                    //{   script : 'src/app/server.js',
+                    //    host: 'http://localhost:3000',
+                    //    port: 3001,
+                    //    env: { NODE_ENV: 'local'}
+                    //}, // or serve static folders '_site' or ['_site','bower_component']
     paths: {
+        /*
+         All paths also have `script`, `styles`, `fonts`, `icons` and `images` properties
+         Feel free to specify a custom path i.e. `scripts: './src/js'`
+         */
         "bower": {
             root: './bower_components',
             fonts: './bower_components/*/dist/fonts'
         },
-        "test": { // where your tests config, specs and reports are saved
-            root: './test',
-            specs: './test/specs',
-            config: './test/karma.conf.js',
-            summary: './test/coverage/summary.json'
-        },
-        "site": { //used to save the compiled demo code + pushed to chosen release destination i.e. AWS
-            root: './_site',
-            scripts: "./_site/scripts",
-            styles: './_site/styles',
-            fonts: './_site/fonts',
-            icons: './_site/icons',
-            images: './_site/images'
-        },
-        "demo": { // files used to demo the source code or an accompanying site
-            root: "./demo",
-            scripts: "./demo/scripts",
-            styles: './demo/styles',
-            fonts: './demo/fonts',
-            icons: './demo/icons',
-            images: './demo/images'
-        },
         source: { //source files to build your component / site
-            root: "./src",
-            scripts: "./src/scripts",
-            styles: './src/styles',
-            fonts: './src/fonts',
-            icons: './src/icons',
-            images: './src/images'
+            root: "./src"
         },
-        dist : { //destination of your compiled source to be redistributed i.e. via bower
-            root: "./dist",
-            scripts: "./dist/scripts",
-            styles: "./dist/styles",
-            fonts: './dist/fonts',
-            icons: './dist/icons',
-            images: './dist/images'
+        "demo": { // files used to demo the source code or an accompanying site.
+            // not files you would want to distribute.
+            root: "./demo"
+        },
+        dist : { // Compiled source code to be redistributed i.e. via bower
+            root: "./dist"
+        },
+        "site": { // Compiled demo code + Compiled source code.
+            // Final build code pushed to your chosen release cloud i.e. AWS
+            root: './_site'
         }
     },
     pkg: pkg
