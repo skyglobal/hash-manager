4e6b16d2701b4f482eab3787f0ecef9140f1e23a
Peter Mouland <peter.mouland@gmail.com>
2015-02-06 16:47:32 +0000

    update requirejs dependency

--- src/scripts/hash-manager.requirejs.js
+++ src/scripts/hash-manager.requirejs.js
@@ -1,7 +1,7 @@
 var local; local['hash-manager'] = require('./hash-manager');
 
 if (typeof window.define === "function" && window.define.amd) {
-    define('bower_components/bskyb-hash-manager/dist/js/hash-manager.requirejs', [], function() {
+    define('bower_components/bskyb-hash-manager/dist/scripts/hash-manager.requirejs', [], function() {
         'use strict';
         return local['hash-manager'];
     });
